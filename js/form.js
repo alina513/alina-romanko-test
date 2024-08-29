cf9833220e36f853fdeceb7d3498ef2eafdc8d92
alina513 <130853290+alina513@users.noreply.github.com>
2024-08-29 15:08:09 +0300

    debug

--- js/form.js
+++ js/form.js
@@ -1,13 +1,9 @@
 (() => {
-    const form = document.querySelector('.contact-form');
-  
-    form.addEventListener('submit', (event) => {
-      event.preventDefault(); 
-  
-      
-      form.reset();
-  
-      
-      window.location.href = 'thankyou.html';
-    });
-  })();
+  const form = document.querySelector(".contact-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    form.reset();
+    window.location.href = "thankyou.html";
+  });
+})();
--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,36 @@
+// (() => {
+//     const menuBtnRef = document.querySelector('[data-menu-button]');
+//     const mobileMenuRef = document.querySelector('[data-menu]');
+
+//     menuBtnRef.addEventListener('click', () => {
+//       const expanded =
+//         menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//       menuBtnRef.classList.toggle('is-open');
+//       menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//       mobileMenuRef.classList.toggle('is-open');
+//       document.body.classList.toggle("no-scroll");
+//     });
+//   })();
+
 (() => {
-    const menuBtnRef = document.querySelector('[data-menu-button]');
-    const mobileMenuRef = document.querySelector('[data-menu]');
-  
-    menuBtnRef.addEventListener('click', () => {
-      const expanded =
-        menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-  
-      menuBtnRef.classList.toggle('is-open');
-      menuBtnRef.setAttribute('aria-expanded', !expanded);
-  
-      mobileMenuRef.classList.toggle('is-open');
-      document.body.classList.toggle("no-scroll");
+  const body = document.body;
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  const navLinksItems = document.querySelectorAll(".nav-links a");
+
+  hamburger.addEventListener("click", () => {
+    const isOpen = hamburger.classList.toggle("is-open");
+    navLinks.classList.toggle("is-open", isOpen);
+    body.classList.toggle("no-scroll", isOpen);
+  });
+
+  navLinksItems.forEach((link) => {
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("is-open");
+      navLinks.classList.remove("is-open");
+      body.classList.remove("no-scroll");
     });
-  })();
-  
+  });
+})();
