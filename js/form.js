cfebb4716a28806ef2fe5a970244c70253a31c39
alina513 <130853290+alina513@users.noreply.github.com>
2024-08-29 13:42:55 +0300

    all

---
+++ js/form.js
@@ -0,0 +1,13 @@
+(() => {
+    const form = document.querySelector('.contact-form');
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); 
+  
+      
+      form.reset();
+  
+      
+      window.location.href = 'thankyou.html';
+    });
+  })();
